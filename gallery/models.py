aabbc7b698bd68f37e6e24c815e4425453a5c61e
Triah <nicolaihedegaardjensen93@gmail.com>
2017-12-22 23:22:13 +0100

    setting up the skeleton for the django page.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.image_id
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Image
+
+
+def gallery(request):
+    gallery_images = Image.objects.all()
+    context = {'gallery_images': gallery_images}
+    return render(request, 'gallery.html', context)
 
 # Create your views here.
