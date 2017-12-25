3e11924764c9f24ccb4ffbee04b920cf5306ed6d
Triah <nicolaihedegaardjensen93@gmail.com>
2017-12-25 23:17:02 +0100

    serving static files, header, footer

--- alexportfolio/settings.py
+++ alexportfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import os.path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/static/"),
-]
+
+STATICFILES_DIRS = (os.path.join('static'), )
