9db70053a7a02926d11d9f10b0993aad63f460cf
Triah <nicolaihedegaardjensen93@gmail.com>
2018-01-06 02:41:40 +0100

    tons of design stuff

---
+++ static/showreel/showreelscripts.js
@@ -0,0 +1,27 @@
+function directwork() {
+    window.location.href = "http://localhost:8000/";
+}
+
+function directshowreel() {
+    window.location.href = "http://localhost:8000/showreel";
+}
+
+function directcontact() {
+    window.location.href = "http://localhost:8000/contact";
+}
+
+function directabout() {
+    window.location.href = "http://localhost:8000/about";
+}
+
+function directvimeo() {
+    window.location.href = "http://www.vimeo.com";
+}
+
+function directinstagram() {
+    window.location.href = "http://www.instagram.com/alexanderjensen91";
+}
+
+function directlinkedin() {
+    window.location.href = "http://www.linkedin.com";
+}
